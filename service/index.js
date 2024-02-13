20f61f0bf7a9344f06fa1950d7f4e267f3977fbc
Chinmaya Kumar Behera <official.chinu2127@gmail.com>
2024-02-14 00:31:44 +0530

    Update index.js

--- service/index.js
+++ service/index.js
@@ -5,7 +5,7 @@ const app = require("./app");
 app.use(cors());
 
 const server = http.createServer(app);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
   cors: {
