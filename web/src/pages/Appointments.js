ebca5128a09f2709cced28a69491d2f0e78e2ffd
Chinmaya Kumar Behera <official.chinu2127@gmail.com>
2024-05-15 23:41:56 +0530

    Update Appointments.js

--- web/src/pages/Appointments.js
+++ web/src/pages/Appointments.js
@@ -4,9 +4,26 @@ import { useRecoilValue } from "recoil";
 import { AuthState } from "../atom/authState";
 import AppointmentsUser from "../views/appointment/AppointmentsUser";
 import AppointmentsWorker from "../views/appointment/AppointmentsWorker";
+import NavigationHandler from "../handler/NavigationHandler";
 
 const DefaultComponent = () => {
-    return <div>Hello this is default component</div>
+  const { navigateToSignInPage } = NavigationHandler();
+    return (
+      <div className="flex items-center justify-center py-20">
+        <div className="p-5 text-center space-y-3 bg-white rounded-lg">
+          <p className="text-gray-600">
+            You are not allowed to access this page. Please login first to
+            access this feature.
+          </p>
+          <button
+            onClick={navigateToSignInPage}
+            className="px-10 py-3 bg-blue-500 text-white hover:bg-blue-400 transition-all duration-300 rounded-lg"
+          >
+            Got to Login page
+          </button>
+        </div>
+      </div>
+    );
 }
 
 
